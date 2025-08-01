33463dcd8c9932d05355ef2f8ac9ce65cd63ad3b
EJHacks <ee1912136@gmail.com>
2025-08-01 11:52:45 -0400

    New features background particle animation. Plans for more features.

---
+++ script.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
+}
+
+function draw() {
+    background(0,10);
+    fill(random(0,256), random(0,256), random(0,256));
+    ellipse(random(width),random(height),5,5)
+    
+}
+
+// You can remove this but this makes it so that the canvas resizes as the window resizes
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+
+
+
+
+
