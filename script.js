1cab9288c3641fc0f68e8a3b1beb18221c8beef7
EJHacks <ee1912136@gmail.com>
2025-08-01 12:41:25 -0400

    update

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
+    background(0); 
+    frameRate(30); 
 }
 
 function draw() {
-    background(0,10);
-    fill(random(0,256), random(0,256), random(0,256));
-    ellipse(random(width),random(height),5,5)
-    
-}
+    fill(0, 10);
+    rect(0, 0, width, height);
 
+    fill(random(255), random(255), random(255), 150);
+    ellipse(random(width), random(height), 5, 5);
+}
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+    background(0); 
 }
-
 
 
 
